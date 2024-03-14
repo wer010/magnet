eff3fc0ce1a3c63d386aa61ae81b5ae2ab5e2433
lanhai <yuri924@163.com>
2024-03-14 18:54:02 +0800

    add closed-form solution

--- em_tracking/korean/test_em_acc.py
+++ em_tracking/korean/test_em_acc.py
@@ -29,11 +29,13 @@ def get_pos_by_matrix_analysis(b_sensor):
 
 def main():
     theory_bt = 2.42e-9
-    data = get_data("20240311_test3.txt")
+    data = get_data("20240311_test1.txt")
     b_sensor = data['magnetic_intensity']
 
     pos_label = data['position']
     r_label = np.linalg.norm(pos_label,axis=-1)
+    x_sign = np.sign(pos_label[:,0])
+
 
     rm = data['rotation_matrix']
 
@@ -56,42 +58,71 @@ def main():
 
     start = datetime.now()
     pos_ana,r_ana = get_pos_by_analytical_solution(b_sensor)
+    pos_ana = pos_ana*[[1,-1,1]]
     end = datetime.now()
-    print(f'Time consumpts {end-start}s.')
+    mae_ana = np.mean(np.abs(pos_label-pos_ana),axis=0)
+    vae_ana = np.var(np.abs(pos_label-pos_ana),axis=0)
+    print(f'Time consumpts {end - start}s. Mean absolute error is {np.mean(np.abs(r_label - r_ana), axis=0)},{mae_ana}, variance of absolute error is {np.var(np.abs(r_label - r_ana), axis=0)},{vae_ana}')
 
     start = datetime.now()
     pos_mat,r_mat = get_pos_by_matrix_analysis(b_sensor_with_sign)
+    x_sign_mat = np.sign(pos_mat[:,0])
+    pos_mat = pos_mat*(x_sign*x_sign_mat)[:,np.newaxis]
     end = datetime.now()
-    print(f'Time consumpts {end - start}s.')
-
-    ax1 = plt.subplot(141)
+    mae_mat = np.mean(np.abs(pos_label - pos_mat), axis=0)
+    vae_mat = np.var(np.abs(pos_label - pos_mat), axis=0)
+    print(f'Time consumpts {end - start}s. Mean absolute error is {np.mean(np.abs(r_label - r_mat), axis=0)},{mae_mat}, variance of absolute error is {np.var(np.abs(r_label - r_mat), axis=0)},{vae_mat}')
+    ax1 = plt.subplot(241)
     ax1.plot(r_ana,label='EM ana')
     ax1.plot(r_mat,label='EM mat')
     ax1.plot(r_label,label='Optical tracking')
     ax1.set_title('Distance')
     ax1.legend()
 
-    ax2 = plt.subplot(142)
+    ax2 = plt.subplot(242)
     ax2.plot(pos_ana[:,0],label='EM ana')
     ax2.plot(pos_mat[:,0],label='EM mat')
     ax2.plot(pos_label[:,0],label='Optical tracking')
     ax2.set_title('x')
     ax2.legend()
 
-    ax3 = plt.subplot(143)
-    ax3.plot(-1*pos_ana[:,1],label='EM ana')
+    ax3 = plt.subplot(243)
+    ax3.plot(pos_ana[:,1],label='EM ana')
     ax3.plot(pos_mat[:,1],label='EM mat')
     ax3.plot(pos_label[:, 1],label='Optical tracking')
     ax3.set_title('y')
     ax3.legend()
 
-    ax4 = plt.subplot(144)
+    ax4 = plt.subplot(244)
     ax4.plot(pos_ana[:,2],label='EM ana')
     ax4.plot(pos_mat[:,2],label='EM mat')
     ax4.plot(pos_label[:, 2],label='Optical tracking')
     ax4.set_title('z')
     ax4.legend()
 
+    ax5 = plt.subplot(245)
+    ax5.plot(np.abs(r_label-r_ana), label='EM ana')
+    ax5.plot(np.abs(r_label-r_mat), label='EM mat')
+    ax5.set_title('Distance Absolute Error')
+    ax5.legend()
+
+    ax6 = plt.subplot(246)
+    ax6.plot(np.abs(pos_label[:, 0]-pos_ana[:, 0]), label='EM ana')
+    ax6.plot(np.abs(pos_label[:, 0]-pos_mat[:, 0]), label='EM mat')
+    ax6.set_title('x Absolute Error')
+    ax6.legend()
+
+    ax7 = plt.subplot(247)
+    ax7.plot(np.abs(pos_label[:, 1]-pos_ana[:, 1]), label='EM ana')
+    ax7.plot(np.abs(pos_label[:, 1]-pos_mat[:, 1]), label='EM mat')
+    ax7.set_title('y Absolute Error')
+    ax7.legend()
+
+    ax8 = plt.subplot(248)
+    ax8.plot(np.abs(pos_label[:, 2]-pos_ana[:, 2]), label='EM ana')
+    ax8.plot(np.abs(pos_label[:, 2]-pos_mat[:, 2]), label='EM mat')
+    ax8.set_title('z Absolute Error')
+    ax8.legend()
 
     plt.show()
 
